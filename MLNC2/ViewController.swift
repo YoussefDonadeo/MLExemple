836bab95e58ae58a81a60cf102a2a2e70835623b
YoussefDonadeo <youssef.donadeo@studenti.unicampania.it>
2019-03-12 15:47:19 +0100

    Initial Commit

---
+++ MLNC2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLNC2
+//
+//  Created by Youssef Donadeo on 12/03/2019.
+//  Copyright © 2019 YoussefDonadeo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MLNC2Tests/MLNC2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MLNC2Tests.swift
+//  MLNC2Tests
+//
+//  Created by Youssef Donadeo on 12/03/2019.
+//  Copyright © 2019 YoussefDonadeo. All rights reserved.
+//
+
+import XCTest
+@testable import MLNC2
+
+class MLNC2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
